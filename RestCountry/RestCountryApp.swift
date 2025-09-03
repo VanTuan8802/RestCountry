ffa9ec7f64d79dd1269c1da84e576cc40d95d5da
VanTuan8802 <vantuan8802@gmail.com>
2025-09-03 18:38:28 +0700

    initt project

---
+++ RestCountry/RestCountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestCountryApp.swift
+//  RestCountry
+//
+//  Created by Tuấn Nguyễn on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct RestCountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
